5106adc8b67d319e5989b33fe6a6ca521847aa00
Yonatan <107502940+Biyuktul@users.noreply.github.com>
2025-05-14 06:26:55 +0300

    modifying

--- database-adv-script/joins_queries.sql
+++ database-adv-script/joins_queries.sql
@@ -2,13 +2,16 @@
 SELECT [user].first_name, Booking.total_price, Booking.status
 FROM Booking INNER JOIN [User]
 ON Booking.[user_id] = [User].[user_id]
+ORDER BY [User].first_name;
 
 -- retrieve all properties and their reviews, including properties that have no reviews.
 SELECT Property.name, Property.location, Review.rating, Review.comment, Review.created_at
 FROM Property LEFT JOIN Review
 ON Property.property_id = Review.property_id
+ORDER BY Review.created_at;
 
 -- retrieve all users and all bookings, even if the user has no booking or a booking is not linked to a user.
 SELECT [User].first_name, [User].last_name, Booking.total_price, Booking.status
 FROM [User] FULL OUTER JOIN Booking
 ON Booking.[user_id] = [User].[user_id]
+ORDER BY [User].first_name;
