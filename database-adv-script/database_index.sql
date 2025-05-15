da55cb1cf34158445b101ccb0619f886a95530e6
Yonatan <107502940+Biyuktul@users.noreply.github.com>
2025-05-15 03:44:44 +0300

    measuring

--- database-adv-script/database_index.sql
+++ database-adv-script/database_index.sql
@@ -3,3 +3,10 @@ CREATE INDEX property_index ON Property([location], pricepernight, host_id);
 CREATE INDEX user_index ON User(first_name);
 CREATE INDEX booking_index ON Booking(property_id, user_id, total_price);
 CREATE INDEX review_index ON Review(rating);
+
+EXPLAIN ANALYZE
+SELECT *
+FROM Booking
+JOIN Property ON Booking.property_id = Property.property_id
+WHERE Booking.created_at >= '2024-01-01'
+ORDER BY Booking.created_at DESC;
