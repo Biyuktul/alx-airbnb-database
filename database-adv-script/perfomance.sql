a90cc953732d1091881f54ea802c35aa93cd502c
Yonatan <107502940+Biyuktul@users.noreply.github.com>
2025-05-15 04:04:23 +0300

    refactoring

--- database-adv-script/perfomance.sql
+++ database-adv-script/perfomance.sql
@@ -9,3 +9,4 @@ LEFT JOIN Property
 ON Booking.property_id = Property.property_id
 LEFT JOIN Payment
 ON Payment.booking_id = Booking.booking_id
+WHERE Payment.payment_id IS NOT NULL AND payment_date > '2025-03-10 15:07:26.273'
