1bd3bb5430cb5cdfb4928ac2523d7527edcff140
Yonatan <107502940+Biyuktul@users.noreply.github.com>
2025-05-15 03:41:38 +0300

    refactoring

--- database-adv-script/database_index.sql
+++ database-adv-script/database_index.sql
@@ -1,5 +1,5 @@
 -- Write SQL CREATE INDEX commands to create appropriate indexes for those columns and save them on database_index.sql
-CREATE INDEX prop_search ON Property([location], pricepernight, host_id);
-CREATE INDEX order_user ON User(first_name);
-CREATE INDEX join_columns ON Booking(property_id, user_id, total_price, [status]);
-			
+CREATE INDEX property_index ON Property([location], pricepernight, host_id);
+CREATE INDEX user_index ON User(first_name);
+CREATE INDEX booking_index ON Booking(property_id, user_id, total_price);
+CREATE INDEX review_index ON Review(rating);
